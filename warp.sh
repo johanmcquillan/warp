93cf892331af7bcc2b41fb9d25f3b81685d18e26
Johan McQuillan <johangmcquillan@gmail.com>
2022-04-04 09:16:04 +0100

    commit warp.sh

---
+++ warp.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# Check dependencies.
+_warp_dependencies=(fzf bat exa isutf8)
+for dependency in $_warp_dependencies
+do
+    if ! which $dependency &> /dev/null
+    then
+        echo "warp: Failed to source: '${dependency}' not installed" 1>&2
+        return 1
+    fi
+done
+
+# _warp prompts the user to choose one of the files or directories.
+function _warp {
+    # To get consistent colours when running `ls` on MacOS we use `gls` if it is installed.
+    local _warp_ls='ls'
+    if which gls &> /dev/null
+    then
+        _warp_ls='gls'
+    fi
+
+    # Use exa for detailed previews inside folders.
+    _warp_exa='exa -la --group-directories-first --color=always --no-filesize --no-user --no-permissions'
+
+    # Run `fzf`.
+    $_warp_ls -ap --group-directories-first --color | \
+        fzf --height 60% --ansi --reverse --cycle \
+        --bind=tab:accept-non-empty,right:accept-non-empty,left:first+down+accept \
+        --preview-window=right:60% \
+        --preview="[ -d {} ] && $_warp_exa {} || ($_warp_exa {} && bat --style=plain --force-colorization --tabs=4 {})"
+}
+
+# _warp_gwd returns a compact format of `pwd`, truncated on the left to the current git repo.
+# Errors if not in git repo.
+function _warp_gwd {
+    gwd="$(git rev-parse --show-toplevel 2> /dev/null)" || return 1
+    local repo_name="$(basename $gwd)"
+    echo "${repo_name}${PWD/#$gwd}/"
+}
+
+# _warp_wd returns a compact format of `pwd`, with `~` shown instead of the full value of $HOME.
+function _warp_wd {
+    local wd="${PWD/#$HOME}"
+    if [[ "$wd" == "$PWD" ]]
+    then
+        # $HOME is not a prefix of $PWD, so just return unchanged $PWD.
+        echo "${PWD}/"
+    else
+        echo "~$wd/"
+    fi
+}
+
+# _warp_pwd returns a compact format of `pwd`.
+function _warp_pwd {
+    _warp_gwd || _warp_wd
+}
+
+# warp is the public function that provides a GUI for navigating a file system.
+# It accepts an optional argument for where to start traversing files.
+function warp {
+    local cursor_up="\033[1A"
+    local erase="\033[K"
+    local prose_color="\033[38;5;35m"
+    local path_color="\033[38;5;69m\033[1m"
+    local reset="\033[0m"
+    if [ -n "$1" ]
+    then
+        cd "$1" || return
+    fi
+    echo
+    while true
+    do
+        echo -e "${cursor_up}${erase}${prose_color}Warping${reset} -> ${path_color}$(_warp_pwd)${reset}"
+        choice=$(_warp)
+        if [[ "$choice" == "./" ]]
+        then
+            # Selected current working directory, so simply exit.
+            return
+        fi
+        if [[ ! -d "$choice" ]]
+        then
+            # Selected a file.
+            if [[ -z "$EDITOR" ]] 
+            then
+                echo "warp: Failed to open $PWD/$choice: "'$EDITOR is not set' 1>&2
+                return 1
+            fi
+            if ! isutf8 "$PWD/$choice" &> /dev/null
+            then
+                echo "warp: Not opening $PWD/$choice: non-text format" 1>&2
+                return
+            fi
+            "$EDITOR" "$PWD/$choice"
+            return
+        fi
+
+        cd "$PWD/$choice" || return
+    done
+}
