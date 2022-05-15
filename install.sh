13f14ec6ced5372e7b3d47c0696adf722ca90896
Johan McQuillan <johangmcquillan@gmail.com>
2022-05-16 00:14:17 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+rc_file=""
+case "$SHELL" in
+    *bash)
+        rc_file=".bashrc"
+        ;;
+    *zsh)
+        rc_file=".zshrc"
+        ;;
+    *)
+        echo "Unsupported shell: $SHELL" &> /dev/stderr
+        echo "Please use either Bash or Zsh" &> /dev/stderr
+        ;;
+esac
+
+curl https://raw.githubusercontent.com/johanmcquillan/warp/master/warp.sh -o "$HOME/.warp.sh"
+
+if [ ! -f "$HOME/$rc_file" ]
+then
+    touch "$HOME/$rc_file"
+fi
+
+# If '$HOME/.warp' appears in the rc file, assume it's already installed.
+grep '$HOME/.warp' "$HOME/$rc_file" &> /dev/null && return
+
+echo '
+if [ -f "$HOME/.warp.sh" ]
+then
+    source "$HOME/.warp.sh"
+fi' >> "$HOME/$rc_file"
